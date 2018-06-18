ed2d5446379847a04bf52cc4c4ddf98e2c8111de
Akshit Arora <36242377+akshitarora1@users.noreply.github.com>
2018-06-18 17:24:14 +0200

    Update Webscrappingproject.R

--- Webscrappingproject.R
+++ Webscrappingproject.R
@@ -4,12 +4,10 @@ ggplot(cars, aes(x = Brand, y = Price)) +geom_boxplot() +
   xlab('Brand') +
   ylab('Price') +
   ggtitle('Best resale price')
-
+#load data set and create a barplot to determine the best brand value.
 
 
 library(highcharter)
-cars<- read.csv(carsolx.csv)
-
-
 hchart(cars, "scatter", hcaes(x = KM_Driven, y = Price , group=Year))
 print(cars)
+#load a new lib and create a scatterplot with 3 variables.
